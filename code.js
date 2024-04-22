df631ea09ca8cef0d327cce33430b8c890ed179d
Savinien <savinienbarbotaud@gmail.com>
2024-04-22 11:28:54 +0200

    better game speed

--- code.js
+++ code.js
@@ -157,7 +157,7 @@ function clearButtonHandler() {
     for (var i = 0; i < cells.length; i++) {
         cells[i].setAttribute("class", "dead");
     }
-    resetGrids;
+    resetGrids();
 }
 
 // start/pause/continue the game
@@ -253,7 +253,6 @@ function countNeighbors(row, col) {
 }
 
 function gliderHandler() {
-    
     createGlider(rows, cols, grid)
 }
 
